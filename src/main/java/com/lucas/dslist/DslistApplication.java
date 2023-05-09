4558fbd63f7f992d27d4c84273a6626d22e763e0
Lucas Muniz <lucaslima1048@gmail.com>
2023-05-09 12:33:24 -0300

    Testing

--- src/main/java/com/lucas/dslist/DslistApplication.java
+++ src/main/java/com/lucas/dslist/DslistApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DslistApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(DslistApplication.class, args);
 	}
